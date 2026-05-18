b3a4fc3274f89e91c495d630e2453b507e7e8f26
Ankit-34095 <132091395+Ankit-34095@users.noreply.github.com>
2026-05-18 10:17:09 +0530

    Updated auth.js

--- hospital-frontend/src/services/auth.js
+++ hospital-frontend/src/services/auth.js
@@ -1,13 +1,13 @@
 import api from './api';
 
 export function login(username, password, role){
-  return api.post('/auth/login', { username, password, role });
+  return api.post('/api/auth/login', { username, password, role });
 }
 
 export function register(data){
-  return api.post('/auth/register', data);
+  return api.post('/api/auth/register', data);
 }
 
 export function registerDoctor(data){
-  return api.post('/auth/register-doctor', data);
+  return api.post('/api/auth/register-doctor', data);
 }
